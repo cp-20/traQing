5d2b4a7188009a55713db3cfa1704ff0bdb7c254
cp-20 <n.naoki.1587@gmail.com>
2024-06-24 00:13:24 +0900

    wip: `<Stat>` コンポーネントを実装

---
+++ apps/client/src/components/Stat.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from 'react';
+
+export type StatProps = {
+  label: ReactNode;
+  value: ReactNode;
+  unit?: ReactNode;
+  annotation?: ReactNode;
+};
+
+export const Stat: FC<StatProps> = ({ label, value, unit, annotation }) => {
+  return (
+    <div className="border rounded-md p-4">
+      <div>
+        <div className="text-sm">{label}</div>
+        <div>
+          <span className="text-3xl text-blue-600 font-medium">{value}</span>
+          {unit && <span>{unit}</span>}
+        </div>
+        {annotation && <div className="text-xs">{annotation}</div>}
+      </div>
+    </div>
+  );
+};
