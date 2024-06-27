596df7b4ecaa63f3f6a54dfa1e84c4add27f2768
cp-20 <n.naoki.1587@gmail.com>
2024-06-28 02:27:02 +0900

    ユーザー詳細ページからトップページに戻る導線をつけた

--- apps/client/src/models/UserDetail.tsx
+++ apps/client/src/models/UserDetail.tsx
@@ -13,7 +13,9 @@ import {
   UserReceivedStampStat,
 } from '@/models/UserStats';
 import { UserTopReactedMessages } from '@/models/UserTopReactedMessages';
+import { IconChevronLeft } from '@tabler/icons-react';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 type UserDetailProps = {
   userId: string;
@@ -30,7 +32,23 @@ export const UserDetail: FC<UserDetailProps> = ({ userId }) => {
 
   return (
     <div className="bg-gray-100 min-h-screen sm:p-8 p-4 flex flex-col sm:gap-8 gap-4">
-      <div className="flex gap-8 justify-center items-center">
+      <div className="lg:hidden flex">
+        <Link
+          to="/"
+          className="px-4 py-2 flex justify-center items-center font-semibold hover:bg-gray-200 duration-200 transition-all rounded-md"
+        >
+          <IconChevronLeft />
+          <span>ホームに戻る</span>
+        </Link>
+      </div>
+      <div className="flex gap-8 justify-center items-center relative">
+        <Link
+          to="/"
+          className="absolute top-1/2 -translate-y-1/2 left-0 px-4 py-2 flex justify-center items-center font-semibold hover:bg-gray-200 duration-200 transition-all rounded-md max-lg:hidden"
+        >
+          <IconChevronLeft />
+          <span>ホームに戻る</span>
+        </Link>
         <div>
           <img
             src={`/api/files/${user.iconFileId}`}
