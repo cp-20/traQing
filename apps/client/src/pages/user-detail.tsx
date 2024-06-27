a9928fa071b46024818a8c93e9c1ff864e973442
cp-20 <n.naoki.1587@gmail.com>
2024-06-28 01:37:45 +0900

    ユーザー画面のローディングを真面目に実装した

--- apps/client/src/pages/user-detail.tsx
+++ apps/client/src/pages/user-detail.tsx
@@ -1,16 +1,39 @@
 import { useUsers } from '@/hooks/useUsers';
 import { UserDetail } from '@/models/UserDetail';
 import { FC } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Loader } from '@mantine/core';
+import clsx from 'clsx';
 
 export const UserDetailPage: FC = () => {
   const params = useParams<{ username: string }>();
-  const { getUserId } = useUsers();
+  const { getUserId, users } = useUsers();
   const userId = getUserId(params.username!);
 
-  if (userId === undefined) {
-    return <>User not found</>;
-  }
-
-  return <UserDetail userId={userId} />;
+  return (
+    <div className="bg-gray-100 min-h-screen relative">
+      <div
+        className={clsx(
+          'grid place-content-center absolute inset-0 bg-gray-100 duration-200 transition-all ease-in',
+          userId && 'invisible opacity-0'
+        )}
+      >
+        <Loader type="bars" size="xl" />
+      </div>
+      {users !== undefined && !userId && (
+        <div className="min-h-screen flex flex-col justify-center items-center gap-4">
+          <div className="text-2xl font-semibold">
+            <span>@{params.username}</span>
+            <span> というユーザーが見つかりません</span>
+          </div>
+          <div>
+            <Link to="/" className="underline text-blue-500">
+              トップページに戻る
+            </Link>
+          </div>
+        </div>
+      )}
+      {userId && <UserDetail userId={userId} />}
+    </div>
+  );
 };
