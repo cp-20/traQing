b030f7da3e5f2768a00e6de46aaa85ae74643f89
cp-20 <n.naoki.1587@gmail.com>
2024-09-21 20:54:51 +0900

    UserAvatar

---
+++ apps/client/src/components/UserAvatar.tsx
@@ -0,0 +1,30 @@
+import { useUsers } from '@/hooks/useUsers';
+import { Skeleton } from '@mantine/core';
+import clsx from 'clsx';
+import { FC } from 'react';
+
+type Props = {
+  userId: string;
+  size?: number;
+} & JSX.IntrinsicElements['img'];
+
+export const UserAvatar: FC<Props> = ({ userId, size = 24, ...props }) => {
+  const { getUserFromId } = useUsers();
+  const user = getUserFromId(userId);
+
+  if (user === undefined) {
+    return <Skeleton circle height={size} />;
+  }
+
+  return (
+    <img
+      src={`/api/files/${user.iconFileId}`}
+      width={size}
+      height={size}
+      loading="lazy"
+      alt=""
+      {...props}
+      className={clsx('rounded-full', props.className)}
+    />
+  );
+};
