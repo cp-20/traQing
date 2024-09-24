bc895b86e0ee21a83a297350b354a8698f5c4f56
cp-20 <n.naoki.1587@gmail.com>
2024-09-24 14:10:54 +0900

    UserAvatar さらに最適化

--- apps/client/src/components/UserAvatar.tsx
+++ apps/client/src/components/UserAvatar.tsx
@@ -6,14 +6,20 @@ type Props = {
   size?: number;
 } & JSX.IntrinsicElements['img'];
 
-export const UserAvatar: FC<Props> = ({ userId, size = 24, ...props }) => (
-  <img
-    src={`/api/avatars/${userId}?width=${size}&height=${size}`}
-    width={size}
-    height={size}
-    loading="lazy"
-    {...props}
-    alt=""
-    className={clsx('rounded-full', props.className)}
-  />
-);
+const sizes = [48, 64, 96, 128, 256, 512, 1024];
+const dpi = 2;
+
+export const UserAvatar: FC<Props> = ({ userId, size = 24, ...props }) => {
+  const fitSize = sizes.find((s) => s >= size * dpi) ?? sizes[sizes.length - 1];
+  return (
+    <img
+      src={`/api/avatars/${userId}?width=${fitSize}&height=${fitSize}`}
+      width={size}
+      height={size}
+      loading="lazy"
+      {...props}
+      alt=""
+      className={clsx('rounded-full', props.className)}
+    />
+  );
+};
