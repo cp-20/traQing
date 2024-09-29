27d8e71e14efa80b897e7631f44c3a280c84e799
cp-20 <n.naoki.1587@gmail.com>
2024-09-29 12:59:29 +0900

    update tailwind color config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -11,7 +13,14 @@ export default {
       '2xl': '1536px',
     },
     colors: {
-      ...require('tailwindcss/colors'),
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
       text: {
         primary: '#49535b',
       },
