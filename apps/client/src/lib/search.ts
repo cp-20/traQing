c237a9040ad76f0cacfacebf47cf41c026f0348a
cp-20 <n.naoki.1587@gmail.com>
2024-11-14 19:10:35 +0900

    search を高速化 (メモ化)

--- apps/client/src/lib/search.ts
+++ apps/client/src/lib/search.ts
@@ -1,8 +1,8 @@
 import type { Channel, Stamp, User, UserGroup } from 'traq-bot-ts';
 
-const sort = (a: string, b: string, keyword: string) => {
-  const aIndex = a.toLowerCase().indexOf(keyword.toLowerCase());
-  const bIndex = b.toLowerCase().indexOf(keyword.toLowerCase());
+const sort = (a: string, b: string, keyword: string, indexOfMap: Map<string, number>) => {
+  const aIndex = indexOfMap.get(a) ?? a.toLowerCase().indexOf(keyword.toLowerCase());
+  const bIndex = indexOfMap.get(b) ?? a.toLowerCase().indexOf(keyword.toLowerCase());
   return aIndex > bIndex
     ? 1
     : aIndex < bIndex
@@ -18,19 +18,37 @@ const sort = (a: string, b: string, keyword: string) => {
               : 0;
 };
 
-export const searchUsers = (users: User[], keyword: string) => {
-  return users
-    .filter((user) => user.name.toLowerCase().includes(keyword.toLowerCase()) || user.displayName.includes(keyword))
-    .sort((a, b) => sort(a.name, b.name, keyword));
+const memorized = <T>(fn: (contents: T[], keyword: string) => T[]) => {
+  const cache = new Map<string, T[]>();
+  return (contents: T[], keyword: string) => {
+    if (!cache.has(`${contents.length}-${keyword}`)) {
+      cache.set(`${contents.length}-${keyword}`, fn(contents, keyword));
+    }
+    // biome-ignore lint/style/noNonNullAssertion: 自明に存在する
+    return cache.get(`${contents.length}-${keyword}`)!;
+  };
 };
 
-export const searchChannels = (channels: Channel[], keyword: string) => {
+export const searchUsers = memorized((users: User[], keyword: string) => {
+  const lowercasedKeyword = keyword.toLowerCase();
+  const keywordIndexOfMap = new Map(users.map((user) => [user.id, user.name.toLowerCase().indexOf(lowercasedKeyword)]));
+  return users
+    .filter((user) => user.name.toLowerCase().includes(lowercasedKeyword) || user.displayName.includes(keyword))
+    .sort((a, b) => sort(a.name, b.name, lowercasedKeyword, keywordIndexOfMap));
+});
+
+export const searchChannels = memorized((channels: Channel[], keyword: string) => {
   const channelMap = new Map(channels.map((channel) => [channel.id, channel]));
   const paths = keyword.split('/');
-  const search = (channels: Channel[], keyword: string) =>
-    channels
-      .filter((channel) => channel.name.toLowerCase().includes(keyword.toLowerCase()))
-      .sort((a, b) => (a.parentId ? 1 : b.parentId ? -1 : sort(a.name, b.name, keyword)));
+  const search = (channels: Channel[], keyword: string) => {
+    const lowercasedKeyword = keyword.toLowerCase();
+    const keywordIndexOfMap = new Map(
+      channels.map((channel) => [channel.id, channel.name.toLowerCase().indexOf(lowercasedKeyword)]),
+    );
+    return channels
+      .filter((channel) => channel.name.toLowerCase().includes(lowercasedKeyword))
+      .sort((a, b) => (a.parentId ? 1 : b.parentId ? -1 : sort(a.name, b.name, lowercasedKeyword, keywordIndexOfMap)));
+  };
   let filtered = channels;
   for (const [i, path] of paths.entries()) {
     filtered = search(filtered, path);
@@ -39,16 +57,25 @@ export const searchChannels = (channels: Channel[], keyword: string) => {
     // biome-ignore lint/style/noNonNullAssertion: 必ず channel は存在する
     filtered = filtered.flatMap((v) => v.children.map((id) => channelMap.get(id)!));
   }
-};
+  return [];
+});
 
-export const searchStamps = (stamps: Stamp[], keyword: string) => {
+export const searchStamps = memorized((stamps: Stamp[], keyword: string) => {
+  const lowercasedKeyword = keyword.toLowerCase();
+  const keywordIndexOfMap = new Map(
+    stamps.map((stamp) => [stamp.id, stamp.name.toLowerCase().indexOf(lowercasedKeyword)]),
+  );
   return stamps
-    .filter((prof) => prof.name.toLowerCase().includes(keyword.toLowerCase()))
-    .sort((a, b) => sort(a.name, b.name, keyword));
-};
+    .filter((prof) => prof.name.toLowerCase().includes(lowercasedKeyword))
+    .sort((a, b) => sort(a.name, b.name, lowercasedKeyword, keywordIndexOfMap));
+});
 
-export const searchGroups = (groups: UserGroup[], keyword: string) => {
+export const searchGroups = memorized((groups: UserGroup[], keyword: string) => {
+  const lowercasedKeyword = keyword.toLowerCase();
+  const keywordIndexOfMap = new Map(
+    groups.map((group) => [group.id, group.name.toLowerCase().indexOf(lowercasedKeyword)]),
+  );
   return groups
-    .filter((group) => group.name.toLowerCase().includes(keyword.toLowerCase()))
-    .sort((a, b) => sort(a.name, b.name, keyword));
-};
+    .filter((group) => group.name.toLowerCase().includes(lowercasedKeyword))
+    .sort((a, b) => sort(a.name, b.name, lowercasedKeyword, keywordIndexOfMap));
+});
