cfa268e543537177f147ebe70541ed1fb8304e35
cp-20 <n.naoki.1587@gmail.com>
2024-11-15 18:42:27 +0900

    EffectIcon 直し

--- apps/client/src/components/icons/EffectIcon.tsx
+++ apps/client/src/components/icons/EffectIcon.tsx
@@ -3,7 +3,7 @@ import type { SVGProps } from 'react';
 export const EffectIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <title>エフェクト</title>
-    <path d="M9.71365 12.1674L12.3989 9.51574L20.4545 17.4707L17.7693 20.1223L9.71365 12.1674Z" fill="#525E67" />
+    <path d="M9.71365 12.1674L12.3989 9.51574L20.4545 17.4707L17.7693 20.1223L9.71365 12.1674Z" fill="currentColor" />
     <rect
       width="3.77381"
       height="3.77381"
