db1f22e0c8120fb409192aae97497c9734b44452
cp-20 <n.naoki.1587@gmail.com>
2024-12-27 14:24:41 +0900

    🍫 🫐 🍯 🍣 🥘 🥪 コードのところが間違っていたのを修正

--- apps/client/src/pages/playground/PlaygroundPage.tsx
+++ apps/client/src/pages/playground/PlaygroundPage.tsx
@@ -189,7 +189,7 @@ export const PlaygroundPage: FC = () => {
         <pre className="border rounded-md p-4">
           <code className="text-wrap break-all">{`const query = ${JSON.stringify(normalizedQuery, null, 2)};
 const params = new URLSearchParams(query);
-const result = await fetch('https://traqing.cp20.dev/api/${apiKind}?\$\{params.toString()\}').then((r) => r.json());
+const result = await fetch(\`https://traqing.cp20.dev/api/${apiKind}?\$\{params.toString()\}\`).then((r) => r.json());
 console.log(result);`}</code>
         </pre>
       </Card>
