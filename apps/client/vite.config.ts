197bcf88dc7864964f35beee70c21bc9f0478d47
cp-20 <n.naoki.1587@gmail.com>
2026-06-12 21:52:34 +0900

    fix: ビルド時に CSS が正しく反映されないバグを修正

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    cssTarget: 'chrome123',
+  },
   server: {
     proxy: {
       '/api': {
